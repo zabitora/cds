ca6c08e1d59eba076b32740e6d4e83cde5865545
zabitora <zabi.tora@gmail.com>
2018-02-16 12:13:55 -0500

    Update filereader.c

--- filereader.c
+++ filereader.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
 int main(){
+    // creating pointer
     FILE *p;
     char ch;
-
+    
+    // Opens file to read
     p = fopen("sample.txt", "r");
 
     while(1){
-        ch = fgetc(p);
-        if(ch==EOF){break;}
+        ch = fgetc(p); // Gets data by char
+        if(ch==EOF){break;} // Checks for any data
         printf("%c", ch);
     }
     return 0;
